a1db191bf0d688656edd2e335c0ac648321f407b
BlueVM <admin@bluevm.com>
2013-05-15 20:09:08 -1000

    Updates to neon.

--- data/update/update.sh
+++ data/update/update.sh
@@ -1,2 +1,6 @@
-echo "No recent changes to structure."
-echo "* * * * * sh /var/neon/data/scripts/stats.sh " | crontab -
+mkdir ~/neon-install/
+cd ~/neon-install/
+wget https://raw.github.com/BlueVM/Neon/develop/neonpanel/includes/configs/nginx.neon.conf
+rm -rf /etc/nginx/sites-enabled/nginx.neon.conf 
+mv nginx.neon.conf /etc/nginx/sites-enabled/nginx.neon.conf
+service nginx reload
