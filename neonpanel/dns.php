a0dffbfbe213bfd60b65cf106c4b6e18f6c6c3ae
BlueVM <admin@bluevm.com>
2013-05-22 20:47:16 -1000

    Patches to Domain Data.

--- neonpanel/dns.php
+++ neonpanel/dns.php
@@ -34,8 +34,8 @@ if($LoggedIn === false){
 		$sName = $_POST['name'];
 		$sType = $_POST['type'];
 		$sContent = $_POST['content'];
-		if($sDomain = $database->CachedQuery("SELECT * FROM dns.domains WHERE `name` = :Domain", array(':Domain' => $sDomain), 1)){
-			$sAdd = $database->CachedQuery("INSERT INTO dns.records(domain_id, name, type, content, ttl, prio) VALUES(:DomainId, :Name, :Type, :Content, :TTL, :Prio)", array(':DomainId' => $sDomain->data[0]["id"], ':Name' => $sName, ':Type' => $sType, ':Content' => $sContent, ':TTL' => "120", ':Prio' => "0"), 1);
+		if($sDomainData = $database->CachedQuery("SELECT * FROM dns.domains WHERE `name` = :Domain", array(':Domain' => $sDomain), 1)){
+			$sAdd = $database->CachedQuery("INSERT INTO dns.records(domain_id, name, type, content, ttl, prio) VALUES(:DomainId, :Name, :Type, :Content, :TTL, :Prio)", array(':DomainId' => $sDomainData->data[0]["id"], ':Name' => $sName, ':Type' => $sType, ':Content' => $sContent, ':TTL' => "120", ':Prio' => "0"), 1);
 		}
 	}
 	
