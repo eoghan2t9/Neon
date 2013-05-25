4341dd6dc46d3578dc25b6e0a7372f0bdadb1d87
BlueVM <admin@bluevm.com>
2013-05-25 01:10:24 -1000

    Patches to installer.

--- neon-installer.sh
+++ neon-installer.sh
@@ -9,6 +9,11 @@ exec 3>&1 > ~/neon-install/install.log 2>&1
 # Functions
 ############################################################
 
+function status {
+	echo $1;
+	echo $1 >&3
+}
+
 function install {
 	DEBIAN_FRONTEND=noninteractive apt-get -q -y install "$1"
 	apt-get clean
@@ -23,15 +28,41 @@ function remove {
 	apt-get clean
 }
 
+function slaughter_httpd {
+	pkill apache
+	pkill apache2
+	aptitude -y purge ~i~napache
+	apt-get --purge -y autoremove apache*
+	apt-get remove apache2-utils
+	
+	kill -9 $( lsof -i:80 -t )
+	x=$(($x + 1));
+	
+	update-rc.d -f apache2 remove
+	update-rc.d -f apache remove
+	update-rc.d -f nginx remove
+	update-rc.d -f lighttpd remove
+	update-rc.d -f httpd remove
+}
+
+function check_installs {
+	if ! type -p $1 > /dev/null; then
+		status "Unfortunatly $1 failed to install. Neon install aborting."
+		exit 1
+	fi
+}
+
 function check_sanity {
 	# Do some sanity checking.
 	if [ $(/usr/bin/id -u) != "0" ]
 	then
+		status "Neon must be installed as root. Please log in as root and try again."
 		die 'Neon must be installed as root. Please log in as root and try again.'
 	fi
 
 	if [ ! -f /etc/debian_version ]
 	then
+		status "Neon must be installed as root. Please log in as root and try again."
 		die "Neon must be installed on Debian 6.0."
 	fi
 }
@@ -41,12 +72,12 @@ function die {
 	exit 1
 }
 
-function status {
-	echo $1;
-	echo $1 >&3
-}
-
 check_sanity
+
+
+############################################################
+# Begin Installation
+############################################################
 
 status "====================================="
 status "     Welcome to Neon Installation"
@@ -64,55 +95,39 @@ status "This script will begin installing"
 status "Neon in 10 seconds. If you wish to"
 status "cancel the install press CTRL + C"
 sleep 10
+status "Neon needs a bit of information before"
+status "beginning the installation."
 status " "
-status "Neon is now being installed."
+status "What hostname would you like to use (Example: server.yourdomain.com):"
+read user_host
+
+############################################################
+# Begin Cleanup
+############################################################
+
+status " "
 status "Begining cleanup..."
 
 remove="apache2 apache* apache2* apache2-utils mysql* php* nginx lighttpd httpd* php5-fpm vsftpd proftpd exim qmail postfix sendmail"
 
-pkill apache
-pkill apache2
-aptitude -y purge ~i~napache
-apt-get --purge -y autoremove apache*
-apt-get remove apache2-utils
+slaughter_httpd
+status "Cleanup Phase: 1 of 16"
+
 for program in $remove
 do
 	remove $program
 	x=$(($x + 1));
-	status "Clean Up: $x / 23"
+	status "Cleanup Phase: $x of 16"
 done
 apt-get autoremove
 
-kill -9 $( lsof -i:80 -t )
-x=$(($x + 1));
-status "Clean Up: $x / 21"
+status " "
+status "Cleanup completed."
+status "Beginning installation phase 1 of 2"
 
-kill -9 $( lsof -i:3306 -t )
-x=$(($x + 1));
-status "Clean Up: $x / 23"
-
-update-rc.d -f apache2 remove
-x=$(($x + 1));
-status "Clean Up: $x / 23"
-
-update-rc.d -f apache remove
-x=$(($x + 1));
-status "Clean Up: $x / 23"
-
-update-rc.d -f nginx remove
-x=$(($x + 1));
-status "Clean Up: $x / 23"
-
-update-rc.d -f lighttpd remove
-x=$(($x + 1));
-status "Clean Up: $x / 23"
-
-update-rc.d -f httpd remove
-x=$(($x + 1));
-status "Clean Up: $x / 23"
-
-status "Clean Up Completed."
-status "Starting installation please wait..."
+############################################################
+# Begin Install Phase 1
+############################################################
 
 echo "deb http://repo.neoncp.com/dotdeb stable all" >> /etc/apt/sources.list
 wget http://repo.neoncp.com/dotdeb/dotdeb.gpg
@@ -120,7 +135,7 @@ cat dotdeb.gpg | apt-key add -
 rm -rf dotdeb.gpg
 apt-get update
 y=$(($y + 1));
-status "Install: $y / 31"
+status "Installation-Phase 1 Step $y of 31"
 
 install="nginx php5 vim openssl php5-mysql zip unzip sqlite3 php5-sqlite php5-curl php-pear php5-dev acl libcurl4-openssl-dev php5-gd php5-imagick php5-imap php5-mcrypt php5-xmlrpc php5-xsl php5-fpm libpcre3-dev build-essential php-apc git-core pdns-server pdns-backend-mysql host mysql-server phpmyadmin"
 
@@ -131,51 +146,116 @@ do
 	status "Install: $y / 31"
 done
 
-if ! type -p nginx > /dev/null; then
-    status "Unfortunatly nginx failed to install. Neon install aborting."
-	exit 1
-fi
+############################################################
+# Perform Installation Checks
+############################################################
 
-if ! type -p php > /dev/null; then
-    status "Unfortunatly php5 failed to install. Neon installation aborting."
-	exit 1
-fi
+check_installs nginx
+check_installs php
+check_installs git
+check_installs mysql
 
-if ! type -p git > /dev/null; then
-    status "Unfortunatly git-core failed to install. Neon installation aborting."
-	exit 1
-fi
+############################################################
+# Begin Configuration Phase 1
+############################################################
 
-if ! type -p mysql > /dev/null; then
-    status "Unfortunatly mysql failed to install. Neon installation aborting. (Error: #1)"
-	exit 1
-fi
+status " "
+status "Begining Configuration Phase: 1 of 2"
 
 /etc/init.d/mysql stop
 invoke-rc.d mysql stop
 /etc/init.d/nginx stop
 /etc/init.d/php5-fpm stop
-status "Config: 1 / 13"
+status "Base Config: 1 / 11"
 
-wget https://raw.github.com/BlueVM/Neon/develop/neonpanel/includes/configs/my.cnf
-mv /etc/my.cnf /etc/my.cnf.backup
-mv my.cnf /etc/my.cnf
-status "Config: 2 / 13"
+############################################################
+# Download Neon
+############################################################
 
-/etc/init.d/mysql start
+mkdir /var/neon/
+git clone -b develop https://github.com/BlueVM/Neon.git /var/neon/
+
+cd ~/neon-install/
+status "Base Config: 2 / 11"
+
+############################################################
+# Create Folders
+############################################################
+
+touch /var/neon/data/log.txt
+mkdir /var/neon/neonpanel/uploads
+mkdir /var/neon/neonpanel/downloads
+mkdir /home/root/
+
+cd ~/neon-install/
+status "Base Config: 3 / 11"
+
+############################################################
+# Generate Passwords
+############################################################
+
 mysqlpassword=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};`
+mailpassword=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};`
+
+cd ~/neon-install/
+status "Base Config: 4 / 11"
+
+############################################################
+# Create Neon Configs
+############################################################
+
+cp /var/neon/data/config.example /var/neon/data/config.json
+sed -i 's/databaseusernamehere/root/g' /var/neon/data/config.json
+sed -i 's/databasepasswordhere/'${mysqlpassword}'/g' /var/neon/data/config.json
+sed -i 's/databasepasswordhere/'${mysqlpassword}'/g' /usr/share/phpmyadmin/pma.php
+sed -i 's/databasenamehere/panel/g' /var/neon/data/config.json
+sed -i 's/randomlygeneratedsalthere/'${salt}'/g' /var/neon/data/config.json
+
+ssh-keygen -t rsa -N "" -f ~/neon-install/id_rsa
+mkdir ~/.ssh/
+cat id_rsa.pub >> ~/.ssh/authorized_keys
+mv id_rsa /var/neon/data/
+setfacl -Rm user:www-data:rwx /var/neon/*
+
+cd ~/neon-install/
+status "Base Config: 5 / 11"
+
+############################################################
+# Begin Mysql Configuration
+############################################################
+
+mv /etc/my.cnf /etc/my.cnf.backup
+cp /var/neon/neonpanel/includes/configs/my.cnf /etc/my.cnf
+/etc/init.d/mysql start
+
 salt=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};`
 mysqladmin -u root password $mysqlpassword
 
 while ! mysql -u root -p$mysqlpassword  -e ";" ; do
        status "Unfortunatly mysql failed to install correctly. Neon installation aborting (Error #2)".
 done
-status "Config: 3 / 13"
 
-wget https://raw.github.com/BlueVM/Neon/develop/neonpanel/includes/configs/php.conf
-mv php.conf /etc/php5/fpm/pool.d/www.conf
+mysql -u root --password="$mysqlpassword" --execute="CREATE DATABASE IF NOT EXISTS panel;CREATE DATABASE IF NOT EXISTS dns;"
+mysql -u root --password="$mysqlpassword" panel < /var/neon/data.sql
+
+cd ~/neon-install/
+status "Base Config: 6 / 11"
+
+############################################################
+# Begin PHP Configuration
+############################################################
+
+cp /var/neon/neonpanel/includes/configs/php.conf /etc/php5/fpm/pool.d/www.conf
 mv /etc/php5/conf.d/apc.ini /etc/php5/apc.old
-status "Config: 4 / 13"
+rm -rf /etc/php5/fpm/php.ini
+cp /var/neon/neonpanel/includes/configs/php.ini /etc/php5/fpm/php.ini
+
+cd ~/neon-install/
+status "Base Config: 7 / 11"
+
+############################################################
+# Begin SSL Configuration
+############################################################
 
 mkdir /usr/ssl
 cd /usr/ssl
@@ -183,62 +263,63 @@ openssl genrsa -out neon.key 1024
 openssl rsa -in neon.key -out neon.pem
 openssl req -new -key neon.pem -subj "/C=US/ST=Oregon/L=Portland/O=IT/CN=www.neonpanel.com" -out neon.csr
 openssl x509 -req -days 365 -in neon.csr -signkey neon.pem -out neon.crt
-status "Config: 5 / 13"
 
 cd ~/neon-install/
-wget https://raw.github.com/BlueVM/Neon/develop/neonpanel/includes/configs/nginx.neon.conf
+status "Base Config: 8 / 11"
+
+############################################################
+# Begin Nginx Configuration
+############################################################
+
 rm -rf /etc/nginx/sites-enabled/* 
-mv nginx.neon.conf /etc/nginx/sites-enabled/nginx.neon.conf 
-status "Config: 6 / 13"
+mv /var/neon/neonpanel/includes/configs/nginx.neon.conf /etc/nginx/sites-enabled/nginx.neon.conf 
+setfacl -Rm user:www-data:rwx /var/neon/*
 
-mkdir /var/neon/
-git clone -b develop https://github.com/BlueVM/Neon.git /var/neon/
-status "Config: 7 / 13"
+cd ~/neon-install/
+status "Base Config: 9 / 11"
 
-rm -rf /etc/php5/fpm/php.ini
-cp /var/neon/neonpanel/includes/configs/php.ini /etc/php5/fpm/php.ini
-status "Config: 8 / 13"
+############################################################
+# Begin PHPMyAdmin Configuration
+############################################################
 
 cp /var/neon/neonpanel/includes/configs/pma.php /usr/share/phpmyadmin/
 cp /etc/phpmyadmin/config.inc.php /etc/phpmyadmin/config.old.inc.php
 cp /var/neon/neonpanel/includes/configs/pma.config.inc.php /etc/phpmyadmin/config.inc.php
-status "Config: 9 / 13"
 
-touch /var/neon/data/log.txt
-mkdir /var/neon/neonpanel/uploads
-mkdir /var/neon/neonpanel/downloads
-mkdir /home/root/
-status "Config: 10 / 13"
+cd ~/neon-install/
+status "Base Config: 10 / 11"
 
-/etc/init.d/mysql start
-setfacl -Rm user:www-data:rwx /var/neon/*
-mysql -u root --password="$mysqlpassword" --execute="CREATE DATABASE IF NOT EXISTS panel;CREATE DATABASE IF NOT EXISTS dns;"
-mysql -u root --password="$mysqlpassword" panel < /var/neon/data.sql
-status "Config: 11 / 13"
+############################################################
+# Begin PDNS Configuration
+############################################################
 
-cp /var/neon/data/config.example /var/neon/data/config.json
 mv /etc/powerdns/pdns.conf /etc/powerdns/pdns.old
 cp /var/neon/neonpanel/includes/configs/pdns.conf /etc/powerdns/pdns.conf
-cp /var/neon/data/config.example /var/neon/data/config.json
-sed -i 's/databaseusernamehere/root/g' /var/neon/data/config.json
-sed -i 's/databaseusernamehere/root/g' /etc/powerdns/pdns.conf
-sed -i 's/databasepasswordhere/'${mysqlpassword}'/g' /var/neon/data/config.json
-sed -i 's/databasepasswordhere/'${mysqlpassword}'/g' /usr/share/phpmyadmin/pma.php
-sed -i 's/databasepasswordhere/'${mysqlpassword}'/g' /etc/powerdns/pdns.conf
-sed -i 's/databasenamehere/panel/g' /var/neon/data/config.json
 sed -i 's/databasenamehere/dns/g' /etc/powerdns/pdns.conf
-sed -i 's/randomlygeneratedsalthere/'${salt}'/g' /var/neon/data/config.json
-status "Config: 12 / 13"
+sed -i 's/databasepasswordhere/'${mysqlpassword}'/g' /etc/powerdns/pdns.conf
+sed -i 's/databaseusernamehere/root/g' /etc/powerdns/pdns.conf
 
-ssh-keygen -t rsa -N "" -f ~/neon-install/id_rsa
-mkdir ~/.ssh/
-cat id_rsa.pub >> ~/.ssh/authorized_keys
-mv id_rsa /var/neon/data/
-setfacl -Rm user:www-data:rwx /var/neon/*
-status "Config: 13 / 13"
+cd ~/neon-install/
+status "Base Config: 11 / 11"
 
-aptitude -y purge ~i~napache
+############################################################
+# Begin Installation Phase 2 of 2
+############################################################
+
+
+
+############################################################
+# Begin Configuration Phase 2 of 2
+############################################################
+
+
+
+############################################################
+# Begin Clean Up
+############################################################
+
 status "Finishing and cleaning up..."
+aptitude -y purge ~i~napache
 /etc/init.d/nginx start
 /etc/init.d/pdns start
 /etc/init.d/php5-fpm start
