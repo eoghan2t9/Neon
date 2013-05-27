08f297b6ecc0acd7b5a19899a1b35b76d7f035b0
BlueVM <admin@bluevm.com>
2013-05-27 01:42:35 -1000

    Patches to make the installer better for the end user.

--- neon-installer.sh
+++ neon-installer.sh
@@ -65,12 +65,6 @@ function check_sanity {
 		status "Neon must be installed as root. Please log in as root and try again."
 		die "Neon must be installed on Debian 6.0."
 	fi
-	
-	if [[ $HOSTNAME != *.* ]]
-	then
-        die "You must set a valid hostname (server.example.com) before installing Neon. You can do this with the command: hostname server.example.com"
-	fi
-
 }
 
 function die {
